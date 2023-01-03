338dc8a08b356d1aa6125af56e46f7d02cc5637a
AshokTheja <ashokthejab@gmail.com>
2023-01-03 16:39:23 +0530

    Delete columns and sort

---
+++ src/Easy/DeleteColumnsandSort.java
@@ -0,0 +1,55 @@
+package Easy;
+
+import java.util.Arrays;
+
+public class DeleteColumnsandSort {
+
+	public int minDeletionSize(String[] strs) {
+
+		String[] here1 = new String[strs[0].length()];
+		int now1 = 0;
+
+		for (int j = 0; j <= strs[0].length() - 1; j++) {
+
+			String now = "";
+
+			for (int i = 0; i <= strs.length - 1; i++) {
+
+				now = now + strs[i].charAt(j) + "";
+
+			}
+
+			here1[j] = now;
+
+		}
+
+		for (int k = 0; k <= here1.length - 1; k++) {
+
+			String nowHere = here1[k];
+			char[] ch = nowHere.toCharArray();
+			Arrays.sort(ch);
+			String hh = new String(ch);
+
+			if ((hh.equals(here1[k]))) {
+				continue;
+			} else {
+				now1 += 1;
+			}
+
+		}
+
+		return now1;
+
+	}
+
+	public static void main(String[] args) {
+
+		DeleteColumnsandSort ds = new DeleteColumnsandSort();
+
+		String[] here = { "a", "b" };
+
+		System.out.println(ds.minDeletionSize(here));
+
+	}
+
+}
